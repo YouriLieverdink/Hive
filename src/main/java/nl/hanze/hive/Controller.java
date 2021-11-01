19cac3acd6905187bda37dd776be7f236693f326
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2021-11-01 21:32:38 +0100

    refactor: clean the controller class

--- src/main/java/nl/hanze/hive/Controller.java
+++ src/main/java/nl/hanze/hive/Controller.java
@@ -1,34 +1,37 @@
 package nl.hanze.hive;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Controller implements Hive {
 	/**
-	 * The hand of the white player.
+	 * The hands of the active players.
 	 */
-	private Hand white;
+	private Map<Player, Hand> hands;
 
 	/**
-	 * The hand of the black player.
-	 */
-	private Hand black;
-
-	/**
-	 * The board.
+	 * The active board.
 	 */
 	private Board board;
 
 	/**
-	 * The current player.
+	 * The player whose turn it is.
 	 */
-	private Player current;
+	private Player turn;
 
 	/**
 	 * Class constructor.
 	 */
 	public Controller() {
-		white = new Hand(Player.WHITE);
-		black = new Hand(Player.BLACK);
+		// Initialise the hands of the players.
+		hands = new HashMap<>();
+		for (Player p : Player.values()) {
+			hands.put(p, new Hand(p));
+		}
+
+		// Create the board and set the turn.
 		board = new Board();
-		this.current = Player.WHITE;
+		turn = Player.WHITE;
 	}
 
 	/**
@@ -37,35 +40,26 @@ public class Controller implements Hive {
 	 * @param board The board to use.
 	 */
 	public Controller(Board board) {
-		white = new Hand(Player.WHITE);
-		black = new Hand(Player.BLACK);
+		this();
+
 		this.board = board;
-		this.current = Player.WHITE;
 	}
 
 	@Override
 	public void play(Tile tile, int q, int r) throws IllegalMove {
 		// Create the stone to play.
-		Stone stone = new Stone(current, tile);
+		Stone stone = new Stone(turn, tile);
 
-		if (current == Player.WHITE) {
-
-			if (!white.remove(stone)) {
-				throw new IllegalMove();
-			}
-
-			board.add(new Position(q, r), stone);
-		} //
-		else if (current == Player.BLACK) {
-
-			if (!black.remove(stone)) {
-				throw new IllegalMove();
-			}
-
-			board.add(new Position(q, r), stone);
+		if (!hands.get(turn).remove(stone)) {
+			// The player does not have the stone.
+			throw new IllegalMove();
 		}
 
-		current = getOpponent(current);
+		// The player has the stone, add it to the board.
+		board.add(new Position(q, r), stone);
+
+		// Set the turn to the opponent.
+		turn = opponent(turn);
 	}
 
 	@Override
@@ -79,7 +73,7 @@ public class Controller implements Hive {
 	@Override
 	public boolean isWinner(Player player) {
 		// Retrieve the position of the opponent's queen bee.
-		Stone stone = new Stone(getOpponent(player), Tile.QUEEN_BEE);
+		Stone stone = new Stone(opponent(player), Tile.QUEEN_BEE);
 		Position position = board.getPosition(stone);
 
 		// Check all the neighbouring positions.
@@ -104,7 +98,7 @@ public class Controller implements Hive {
 	 * @param player The player.
 	 * @return The opponent of the player.
 	 */
-	public Player getOpponent(Player player) {
+	public Player opponent(Player player) {
 		return player == Player.WHITE ? Player.BLACK : Player.WHITE;
 	}
 }
