81b455b5929801581aea992b86925f594d426895
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2021-11-06 15:55:44 +0100

    refactor: remove unused imports

--- src/test/java/nl/hanze/hive/BoardCheck.java
+++ src/test/java/nl/hanze/hive/BoardCheck.java
@@ -1,10 +1,8 @@
 package nl.hanze.hive;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
